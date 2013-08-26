7b84375d449d7c5d71e111e15210757c7aae9324
Daniel Ortiz <jdoavila@gmail.com>
2013-08-26 10:46:55 -0600

    Primer Commit
    Primer Commit del sitio.

---
+++ televida63/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+#from emet.forms import ActasPresidentesForm, ActasDiputadosForm, ActasAlcaldesForm
+from django.utils import simplejson
+#from emet.models import RepPresidentes, Movimientos, ActasPresidentes, ActasAlcaldes, RepAlcaldes, RepDiputados, ActasDiputados
+from datetime import datetime
+from django.core import serializers
+
+def inicio(request):
+	#formulario = AuthenticationForm()
+	#return render_to_response('home.html', {'formulario':formulario}, context_instance=RequestContext(request))
+	pass

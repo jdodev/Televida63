a1776457bb63432343e770f2d25034123d5fb9c6
Daniel Ortiz <jdoavila@gmail.com>
2013-09-02 12:27:38 -0600

    Se agrego tiny_mce a la administracion de django

--- televida63/admin.py
+++ televida63/admin.py
@@ -1,10 +1,15 @@
 from televida63.models import Periodista, TiposNoticia, Noticia, Programa, Programacion, Banners, BlogEntrada
 from django.contrib import admin
 
+class Editorcito(admin.ModelAdmin):
+	class Media:
+		js = ('../static/js/tiny_mce/tiny_mce.js',
+			'../static/js/editores/textareas.js')
+
 admin.site.register(Periodista)
 admin.site.register(TiposNoticia)
-admin.site.register(Noticia)
+admin.site.register(Noticia, Editorcito)
 admin.site.register(Programa)
 admin.site.register(Programacion)
 admin.site.register(Banners)
-admin.site.register(BlogEntrada)
+admin.site.register(BlogEntrada, Editorcito)
